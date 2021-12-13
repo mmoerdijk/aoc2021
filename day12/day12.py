fd3a1ba591d08c14d2cca07801ef807cdbb77869
Mart Moerdijk <martmoerdijk@gmail.com>
2021-12-13 21:17:02 +0100

    day12: a bit more state 4 times faster

--- day12/day12.py
+++ day12/day12.py
@@ -1,5 +1,3 @@
-from copy import copy
-from collections import Counter
 import time
 
 # Load data
@@ -13,33 +11,44 @@ for line in input_file.read().split("\n"):
     nodes[current_nodes[1]].add(current_nodes[0])
 
 
-def connect_path(paths, n):
+def connect_path(paths, one_double=True):
     res = []
-    for p in paths:
+    for p, double in paths:
         # Get connections for node
         connections = nodes[p[-1]]
         valid_connections = []
         for c in connections:
-            # Check if lower is maximum 1 lower case node that has n connections or start node
-            if c.islower() and c in p and max(Counter([i for i in p if i.islower()]).values()) == n or c == "start":
+            d = double
+            if c == "start":
                 continue
+            # Check if lower is maximum 1 lower case node that has n connections or start node
+            if c.islower() and c in p:
+                if d or not one_double:
+                    continue
+                d = True
+
             # end of the road for this path
             if c == "end":
-                res.append(copy(p) + [c])
+                res.append(([], d))
+                # res.append([])
                 continue
             # valid connection
-            valid_connections.append(c)
+            valid_connections.append((c, d))
         # Make new paths
-        new_paths = [copy(p) + [vc] for vc in valid_connections]
+        new_paths = [(p + [vc], dc) for vc, dc in valid_connections]
         # and continue ...
-        res += connect_path(paths=new_paths, n=n)
+        res += connect_path(paths=new_paths, one_double=one_double)
     return res
 
+
+
+
 ts = time.time()
-print(f"Answer 1: {len(connect_path(paths=[['start']], n=1)) : >10} Duration:{round(time.time()-ts,2): >5} ")
+print(
+    f"Answer 1: {len(connect_path(paths=[(['start'], False)], one_double=False)) : >10} Duration:{round(time.time() - ts, 2): >5} ")
 ts = time.time()
-print(f"Answer 2: {len(connect_path(paths=[['start']], n=2)) : > 10} Duration:{round(time.time()-ts,2) : >5}")
+print(f"Answer 2: {len(connect_path(paths=[(['start'], False)], one_double=True)) : > 10} Duration:{round(time.time()-ts,2) : >5}")
 
 # Output
-# Answer 1:       4411 Duration: 0.14
-# Answer 2:     136767 Duration: 4.03
+# Answer 1:       4411 Duration: 0.02
+# Answer 2:     136767 Duration: 0.85
