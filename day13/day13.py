e606b18c132bd3b7545b0f67e4a889779f228e40
Mart Moerdijk <mmoerdijk@smart-robotics.nl>
2021-12-13 07:38:42 +0100

    day: 13

---
+++ day13/day13.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+# Load data
+input_file = open("input.txt")
+dots_raw, folds_raw = input_file.read().split("==")
+
+dots = []
+for dot in dots_raw.split("\n"):
+    if dot not in ["", "\n"]:
+        dots += [[int(x) for x in dot.split(",")]]
+
+folds = []
+for fold in folds_raw.split("\n"):
+    if fold not in ["", "\n"]:
+        fold = fold.replace("fold along ", "").split("=")
+        folds += [[fold[0], int(fold[1])]]
+
+dots = np.array(dots)
+grid_size = dots.max(axis=0) + 1
+
+grid = np.zeros((grid_size[1], grid_size[0]))
+
+# mark dots
+for dot in dots:
+    grid[dot[1], dot[0]] = 1
+
+# fold
+for axis, line in folds:
+
+    if axis == "x":
+        flap = np.flip(grid[:, line + 1 :], axis=1)
+        grid[:, :line] += flap[:, :line]
+        grid = grid[:, :line]
+
+    if axis == "y":
+        flap = np.flip(grid[line + 1 :, :], axis=0)
+        grid[:line, :] += flap[:line, :]
+        grid = grid[:line, :]
+
+
+grid[grid > 0] = 1
+
+for i in range(grid.shape[0]):
+    print(list(grid[i]))
+
+print(f"Answer 1: {grid.sum()} ")
